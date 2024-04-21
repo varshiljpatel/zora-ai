7f091b35f3f9b9cba4b1f6bb4ff051452e49a768
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-04-22 05:25:11 +0530

    fix: /email

--- src/app/(app)/email/page.tsx
+++ src/app/(app)/email/page.tsx
@@ -3,19 +3,24 @@
 import Navbar from "@/components/navbar/Navbar";
 import IconButton from "@/components/ui/buttons/IconButton";
 import { ContentCopySharp, CopyAllSharp } from "@mui/icons-material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const EmailPage = () => {
-	const emailData = localStorage.getItem("email-data");
-	const formattedEmailData: string = emailData
+        const [email, setEmail] = useState("");
+	
+        useEffect(() => {
+		const emailData = typeof window !== undefined ? localStorage.getItem("email-data") : "";
+	        const formattedEmailData: string = emailData
 		? emailData.replace(/\n/g, "<br>")
 		: "";
+		setEmail(formattedEmailData);
+	}, []);
 
 	return (
 		<div className="sm:p-8 p-4 h-dvh">
 			<Navbar />
 			<div className="sm:py-8 py-4 flex sm:gap-y-8 gap-y-4 flex-col items-end">
-				<p dangerouslySetInnerHTML={{ __html: formattedEmailData }}></p>
+				<p dangerouslySetInnerHTML={{ __html: email }}></p>
         <IconButton icon={<ContentCopySharp />}>COPY</IconButton>
 			</div>
 		</div>
