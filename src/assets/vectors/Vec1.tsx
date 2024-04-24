9bbb2960e3aa39f519ad83055b0f5568808ded95
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-04-24 10:45:48 +0530

    Update Vec1.tsx

--- src/assets/vectors/Vec1.tsx
+++ src/assets/vectors/Vec1.tsx
@@ -11,43 +11,43 @@ const Vec1 = (props: { height?: number; strokeWidth?: number }) => {
 			<path
 				d="M55.0605 23.4794H77.0404"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M66.4299 34.4693V12.4895"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M156.338 23.4794H178.317"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M167.717 34.4693V12.4895"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M143.589 5.49588H93.675V41.4629H143.589V5.49588Z"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M93.675 18.454H143.579"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M107.632 1H129.612"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
@@ -57,43 +57,43 @@ const Vec1 = (props: { height?: number; strokeWidth?: number }) => {
 			<path
 				d="M241 5.49588H191.086V41.4629H241V5.49588Z"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M191.076 5.49588L216.033 22.9799L240.99 5.49588"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M1 41.4629V5.49588H22.9399V41.4629"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M22.9399 23.4794H1"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M38.4255 5.49588V41.4629"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M30.6829 41.4629V37.9661"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 			<path
 				d="M46.1685 37.9661V41.4629"
 				stroke="black"
-				strokeWidth={props.strokeWidth || "2px"}
+				strokeWidth={props.strokeWidth || "3px"}
 				strokeMiterlimit="10"
 			/>
 		</svg>
