4637b7b2d6ceed872a065b6419aa78e529f08ae4
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-04-24 09:56:25 +0530

    Update Suggestions.tsx

--- src/components/suggestion/Suggestions.tsx
+++ src/components/suggestion/Suggestions.tsx
@@ -4,10 +4,10 @@ import Suggestion from "./Suggestion";
 const Suggestions = (props: { suggestions: string[] }) => {
 	return (
 		<div className="sm:w-4/6 mx-8 flex flex-col items-center sm:gap-y-6 gap-y-4">
-			<p className="text-[26px] tracking-tighter leading-[52.5px] text-center font-normal">
+			<p className="text-[26px] tracking-tight text-center font-normal">
 				Explore suggestions
 			</p>
-			<span className="w-24 h-[2px] rounded-full bg-dark"></span>
+			<span className="w-24 mb-4 h-[2px] rounded-full bg-dark"></span>
 
 			{props.suggestions.map((suggestion, index) => {
 				return <Suggestion text={suggestion.trim()} key={index} />;
