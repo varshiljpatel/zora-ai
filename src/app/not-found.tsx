e4b8c8f430adeb168b52021e09eae4f9afe41f15
anascen <webovein@gmail.com>
2024-04-27 00:57:16 +0530

    feat: 404 add

---
+++ src/app/not-found.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Logo from "@/assets/logo/Logo";
+import { stringConfig } from "@/config/strings";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const NotFound404 = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(async () => {
+            await router.push("/");
+        }, 100000000000);
+    }, []);
+
+    return (
+        <div className="h-dvh m-8 w-full flex flex-col items-center justify-center gap-y-8">
+            <span>
+                <Logo height={26} />
+            </span>
+            <h1 className="font-mono">Oops! Page not found.</h1>
+            <p className="text-center">
+                {stringConfig.title} couldn't find the page you were looking
+                for. {stringConfig.title} will redirects you to "/"{" "}
+                <Link
+                    href={"/"}
+                    className="font-medium underline underline-offset-4"
+                >
+                    Home
+                </Link>{" "}
+                page.
+            </p>
+        </div>
+    );
+};
+
+export default NotFound404;
