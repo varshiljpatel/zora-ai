970603e09e109f7c5546aa552075b2af94eddf09
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-04-27 16:28:32 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,8 +19,7 @@ const config: Config = {
                     DEFAULT: "rgb(0, 0, 0)",
                 },
                 light: {
-                    50: "rgb(215,215,215)",
-                    100: "rgb(239,239,239)",
+                    100: "rgb(230, 230, 230)",
                     DEFAULT: "rgb(255, 255, 255)",
                 },
             },
