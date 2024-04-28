f65b84e156ef2504176b23eebde9d7deadf12775
anascen <webovein@gmail.com>
2024-04-28 15:07:59 +0530

    fix: copy button

--- src/components/ui/buttons/CopyButton.tsx
+++ src/components/ui/buttons/CopyButton.tsx
@@ -6,34 +6,41 @@ import { ContentCopySharp, DoneSharp } from "@mui/icons-material";
 import copy from "copy-to-clipboard";
 
 interface ICopyButton {
-	text: string | any;
+    text: string | any;
 }
 
 const CopyButton = (props: ICopyButton) => {
-	const [isCopied, setIsCopied] = useState(false);
+    const [isCopied, setIsCopied] = useState(false);
 
-	const handleClick: React.MouseEventHandler = async () => {
-		if (isCopied) return;
-		await copy(props.text, {
-      format: "text/plain"
-    });
-		setIsCopied(true);
-		setTimeout(() => {
-			setIsCopied(false);
-		}, 3000);
-		return;
-	};
+    const handleClick: React.MouseEventHandler = async () => {
+        if (isCopied) return;
+        await copy(props.text, {
+            format: "text/plain",
+        });
+        setIsCopied(true);
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 3000);
+        return;
+    };
 
-	return (
-		<>
-			<IconButton
-				onClick={handleClick}
-				icon={isCopied ? <DoneSharp fontSize="small" /> : <ContentCopySharp fontSize="small" />}
-			>
-				{isCopied ? "COPIED" : "COPY"}
-			</IconButton>
-		</>
-	);
+    return (
+        <>
+            <IconButton
+                devider={true}
+                onClick={handleClick}
+                icon={
+                    isCopied ? (
+                        <DoneSharp fontSize="small" />
+                    ) : (
+                        <ContentCopySharp fontSize="small" />
+                    )
+                }
+            >
+                {isCopied ? "COPIED" : "COPY"}
+            </IconButton>
+        </>
+    );
 };
 
 export default CopyButton;
